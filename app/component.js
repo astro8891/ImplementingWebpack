415c34f8f4c4bf5cbb3115b9a24ebccc3b67c50c
Ashley <ashley.j.stewart@live.com>
2015-05-15 22:52:23 +1200

    Eslint Added

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function() {
 	var element = document.createElement('h1');
-	element.innerHTML = 'Hello world and check that this works';
+	element.innerHTML = 'Hello World';
 	return element;
 };
